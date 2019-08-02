ea6f203693622ba277e404be035867675dcde83c
rishabh <7551150+rishabh@users.noreply.github.com>
2019-08-02 00:42:32 -0400

    Fix the scopes, please work.

--- src/opentracing_agent.cpp
+++ src/opentracing_agent.cpp
@@ -17,6 +17,28 @@ namespace ot = opentracing;
 namespace datadog {
 namespace opentracing {
 
+void print_plt_entries(const char *filename) {
+  plthook_t *plthook;
+  unsigned int pos = 0; /* This must be initialized with zero. */
+  const char *name;
+  void **addr;
+
+  if (plthook_open(&plthook, filename) != 0) {
+    printf("plthook_open error: %s\n", plthook_error());
+  }
+
+  while (plthook_enum(plthook, &pos, &name, &addr) == 0) {
+    printf("%p(%p) %s\n", addr, *addr, name);
+  }
+  plthook_close(plthook);
+}
+
+void setupInstrumenter(std::shared_ptr<ot::Tracer> tracer) {
+  tracer_auto_instrument = tracer;
+  // Write the instrumentation code
+  datadog::opentracing::print_plt_entries("libdd_opentracing.so.0");
+}
+
 std::shared_ptr<ot::Tracer> makeTracer(const TracerOptions &options) {
   static bool auto_tracer = false;
   auto maybe_options = applyTracerOptionsFromEnvironment(options);
@@ -38,7 +60,7 @@ std::shared_ptr<ot::Tracer> makeTracer(const TracerOptions &options) {
     if (auto_tracer) {
       throw "Can only set up one instrumenter";
     }
-    setupInstrumenter(tracer);
+    datadog::opentracing::setupInstrumenter(tracer);
     auto_tracer = true;
   }
 
@@ -46,28 +68,6 @@ std::shared_ptr<ot::Tracer> makeTracer(const TracerOptions &options) {
 }
 
 static std::shared_ptr<ot::Tracer> tracer_auto_instrument = nullptr;
-
-void setupInstrumenter(std::shared_ptr<ot::Tracer> tracer) {
-  tracer_auto_instrument = tracer;
-  // Write the instrumentation code
-  print_plt_entries("libdd_opentracing.so.0");
-}
-
-void print_plt_entries(const char *filename) {
-  plthook_t *plthook;
-  unsigned int pos = 0; /* This must be initialized with zero. */
-  const char *name;
-  void **addr;
-
-  if (plthook_open(&plthook, filename) != 0) {
-    printf("plthook_open error: %s\n", plthook_error());
-  }
-
-  while (plthook_enum(plthook, &pos, &name, &addr) == 0) {
-    printf("%p(%p) %s\n", addr, *addr, name);
-  }
-  plthook_close(plthook);
-}
 
 }  // namespace opentracing
 }  // namespace datadog
