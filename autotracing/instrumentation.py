341df24a0d26f905254a3108d1e3b5c33d382aad
rishabh <7551150+rishabh@users.noreply.github.com>
2019-08-02 15:22:59 -0400

    Don't mix JS and Python

--- autotracing/instrumentation.py
+++ autotracing/instrumentation.py
@@ -1,20 +1,19 @@
 
-def finishSpan (spanName) {
+def finishSpan (spanName):
   return '{0}->Finish()\n'.format(spanName)
-}
 
-def pre_curl_easy_perform (arguments, spanName): {
+
+def pre_curl_easy_perform (arguments, spanName):
   return 'auto {0} = tracer->startSpan("curl.request");\n'.format(spanName)
-}
 
-def post_curl_easy_perform (arguments, spanName): {
+def post_curl_easy_perform (arguments, spanName):
   # first argument is the functionName
   curlHandle = arguments[1]
   postCall = '\n{0}->setTag("resource", {1}->change.url);\n'.format(spanName, curlHandle, fin)
   return postCall
-}
 
-class Instrumenter {
+
+class Instrumenter:
 
   def __init__(self):
     self.instrumentations = {
@@ -24,9 +23,7 @@ class Instrumenter {
       },
     }
 
-  def instrument(self, arguments, spanName) {
+  def instrument(self, arguments, spanName):
     functionName = arguments[0]
     patcher = self.instrumentations[functionName]
     return patcher.preCall(arguments, spanName), (patcher.postCall(arguments, spanName) + finishSpan(spanName))
-  }
-}
