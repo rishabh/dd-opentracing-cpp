c2438e2aa97b5d24860d1dacd108bee54fb429d5
Taylor Burmeister <taylor.burmeister@datadoghq.com>
2019-08-02 16:37:35 -0400

    add second curl request

--- examples/cpp-tracing/automated_tracing_curl/src/tracer_example.cpp
+++ examples/cpp-tracing/automated_tracing_curl/src/tracer_example.cpp
@@ -33,6 +33,14 @@ int main(int argc, char* argv[]) {
     if (res != CURLE_OK)
       fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
 
+	curl_easy_setopt(curl, CURLOPT_URL, "https://nytimes.com");
+
+    /* Perform the request, res will get the return code */
+    res = curl_easy_perform(curl);
+    /* Check for errors */
+    if (res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+
     /* always cleanup */
     curl_easy_cleanup(curl);
 
